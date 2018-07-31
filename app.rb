98cea7cc3f1030aa4fe56ed0ae61c6053bca2c37
Jill  Rayca <rkrayca@yahoo.com>
2018-07-31 17:47:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+#homepage- whatever my local host is
+  get '/' do
+    "Hello World"
+  end
+  
+#local host/name
+  get '/name' do
+    "My name is Jill"
+  end 
+  
 end
